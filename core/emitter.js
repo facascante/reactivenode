1d8fcfdb94c5a4974d61a3b2933aecddd757b043
Time Lord <chito.cascante@gmail.com>
2017-02-03 18:09:02 +0800

    support parallel responses

--- core/emitter.js
+++ core/emitter.js
@@ -15,12 +15,44 @@ let emitter = new AppEmitter();
  * 		payload object to be pass on to hook
  * @param {function} cb
  * 		callback function to catch async response from these hook
+ * 
+ * @return 
  **/
 emitter.invokeHook = function(hn,options,cb){
 	
 	let unique_id = shortid.generate();
+	let ctrRspns = 0;
+	let responses = [];
+	let errors = [];
+	
 	options.uid = unique_id;
-	emitter.once(hn + '_response_' + options.uid,cb);
+	
+	/**
+	 * Ensure that each request will receive a proper response 
+	 * by making the reponse event name unique per each request
+	 * */
+	
+	emitter.on(hn + '_response_' + options.uid,function(err,response){
+		
+		let cntLsnr = emitter.listenerCount(hn + '_request');
+		ctrRspns++;
+		
+		if(response){
+			responses.push(response);
+		}
+		if(err){
+			errors.push(err);
+		}
+		
+		/**
+		 * Respond only once all the listener respond
+		 * */
+		if(ctrRspns >=cntLsnr){
+			cb((errors.length > 1) ? errors : err,(responses.length > 1) ? responses : response);
+		}
+		
+	});
+	
 	emitter.emit(hn + '_request',options);
 };
 
@@ -35,9 +67,11 @@ emitter.invokeHook = function(hn,options,cb){
 emitter.registerHook = function(hn,cb){
 	
 	emitter.on(hn + '_request',function(options){
+		
 		cb(options,function(err,content){
 			emitter.emit(hn + '_response_' + options.uid,err,content);
 		});
+		
 	});
 };
 
